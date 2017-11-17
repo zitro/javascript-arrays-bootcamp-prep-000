7e69a718bfee410127cee4660958bcb884154349
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-17 16:20:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,3 +5,7 @@ function addElementToBeginningOfArray(array, element){
 //  array2.unshift(array, element);
 [element, ...array];
 }
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+}
