7b8400bba00ff4ea4da74b287e05875db1ec66ea
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-17 16:08:03 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,7 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat","skittles"];
+
+function addElementToBeginningOfArray(array, element){
+array2 = [];
+  array2.unshift(array, element);
+
+}
